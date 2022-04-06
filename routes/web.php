dc793235272b002128f81a474374f168e38d4425
Nesma <nesmaty2000@gmail.com>
2022-04-05 17:06:44 -0700

    commit message

---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/about', function () {
+   // $name = "nesma";
+   // $age = 21;
+    // return view('about',[
+    //'name' => $name,
+    //'age => $age'
+    //]);
+    //return view('about')->with('name',$name)->with('age',$age);
+  // return view ('about',compact('name','age'));
+
+$name = request('name');
+return view ('about',compact('name'));
+});
+Route::post('/store', function () {
+    $name = request('name');
+    return view('about',compact('name'));
+});
+Route::get('/tasks', function () {
+    /*
+     $tasks = [
+    'task1',
+    'task2',
+    'task3'];
+*/
+  $tasks = [
+    '1'=>'task1',
+    '2'=>'task2',
+    '3'=>'task3'];
+return view ('tasks',compact('tasks'));
+});
+Route::get('/show/{id}', function ($id) {
+    $tasks = [
+        '1'=>'task1',
+        '2'=>'task2',
+        '3'=>'task3'
+    ];
+
+ $task = $tasks[$id];
+return view ('show',compact('task'));
+});
