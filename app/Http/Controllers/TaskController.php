52dfc90784386425f2486cb0dc49399e5b21da87
NesmaYousef <nesmaty2000@gmail.com>
2022-04-23 17:14:37 -0700

    last commit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,11 +8,10 @@ use App\Models\Task;
 class TaskController extends Controller
 {
     public function index(){
-        /* note: I used both query builder and eloquent.*/
-        
        /***  $tasks = DB::table('tasks')->get();*/
        //alphabetical order
-       $tasks = Task::orderBy('name')->get()->all();
+      // $tasks = Task::orderBy('name')->get()->all();
+      $tasks = Task :: all()->sortBy("name");
         //to return only tasks like task1:
        /* $tasks = DB::table('tasks')->where('name','like','task1%')->get();*/
         return view('tasks.index', compact('tasks'));
@@ -45,16 +44,19 @@ class TaskController extends Controller
         return redirect()->back();
     }
     public function edit($id){
-        $tasks = DB::table('tasks')->get();
-        $task = DB::table('tasks')->find($id);
+      /*  $tasks = DB::table('tasks')->get();
+        $task = DB::table('tasks')->find($id);*/
+        $tasks = Task :: all()->sortBy("name");
+        $task = Task :: find($id);
 
         return view('tasks.index', compact('task', 'tasks'));
     }
 
     public function update(Request $request, $id){
-        $task = DB::table('tasks')->where('id',$id)->update([
+        /*$task = DB::table('tasks')->where('id',$id)->update([
             'name' => $request->name
-        ]);
+        ]);*/
+        $task = Task :: where('id',$id)->update(['name' => $request->name]);
 
         return redirect('');
     }
